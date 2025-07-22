8f656f01fa728adfcb87e935891db2330a1dbeb6
axlsotoDuoc <ax.sotov@duocuc.cl>
2025-07-22 18:55:55 -0400

    Animations

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,44 +3,12 @@ window.addEventListener('DOMContentLoaded', function () {
 
     window.sr = ScrollReveal();
 
-    sr.reveal(`.navbar`, {
+    sr.reveal(`.navbar-index`, {
         duration: 800,
         origin: `bottom`,
         distance: `-100px`
     });
 
-
-    window.sr = ScrollReveal();
-
-    sr.reveal(`.product-bg`, {
-        duration: 1400,
-        origin: `top`,
-        distance: `-100px`
-    });
-
-    window.sr = ScrollReveal();
-
-    sr.reveal(`.floats`, {
-        duration: 1000,
-        origin: `right`,
-        distance: `-100px`
-    });
-
-
-    $(document).ready(function () {
-        $('.carrusel').slick({
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            adaptiveHeight: true,
-            autoplay: true,
-            autoplaySpeed: 2500,
-            pauseOnHover: false,
-            pauseOnFocus: true,
-            arrows: false
-        });
-    });
 
 });
 
